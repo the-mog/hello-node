abb5be6d5534e8a6897a9ef5fb3c6d916a816028
hans <themog@tari.com>
2020-07-17 00:31:58 +0200

    add to it

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node, party');
+  res.send('Hello Node, wassup');
 });
 
 app.listen(PORT, HOST);
