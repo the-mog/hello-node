ee443e8a4d3f9322991e48ad00bbc74b59c1477f
hans <themog@tari.com>
2020-07-19 22:04:54 +0200

    scan with aquasec

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node, party at your place');
+  res.send('Hello Node, party at your place. bob');
 });
 
 app.listen(PORT, HOST);
